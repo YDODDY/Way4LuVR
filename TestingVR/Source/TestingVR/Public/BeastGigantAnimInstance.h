ddfd0aa22db7fe8456d81b1234fe1bd690089fb0
rucoready <154755749+rucoready@users.noreply.github.com>
2024-05-08 02:33:19 +0900

    보스 리타게팅 및 애니메이션 변경
    c++ 깡통추가

---
+++ TestingVR/Source/TestingVR/Public/BeastGigantAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BeastGigantAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGVR_API UBeastGigantAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
